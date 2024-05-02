110ef9c952ff49a83abf0df40d3dd8c16ec7a25e
ryanchung34 <91290409+ryanchung34@users.noreply.github.com>
2024-05-02 17:28:43 +1200

    Update Project_2.java

--- src/project_2/Project_2.java
+++ src/project_2/Project_2.java
@@ -15,6 +15,8 @@ public class Project_2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
+        System.out.println("Hello World!");
     }
     
 }
