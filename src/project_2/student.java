37a84b043f8c9a86fc614063e01014b6ee5de3f1
ryanchung34 <91290409+ryanchung34@users.noreply.github.com>
2024-06-06 10:30:51 +1200

    Update student.java

--- src/project_2/student.java
+++ src/project_2/student.java
@@ -1,1 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project_2;
+
+/**
+ *
+ * @author Ryan & Donghyun
+ */
+public class Student {
+    
+    private String name;
+    private int age;
+    private char gender;
+    private int id;
+        
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    
+    public Student(String name, int age, char gender, int id) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.id = id;
+    }
+    
+    @Override
+    public String toString(){
+        return "Name: " + name + ", Age: " + age + ", Gender: " + gender + ", ID: " + id; 
+    }
+    
+}
 
